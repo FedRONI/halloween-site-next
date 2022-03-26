5480d7e9f5f510a8b46e2101422cd973f6790e73
Шистеров Федор Павлович <pl8757@C201N1.pl1.edu>
2022-03-26 14:31:02 +0500

    a

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,48 @@
+import Image from 'next/image'
+import Menu from '../components/menu'
+import home1 from '../images/home1-img.png'
+
 export default function Index() {
   return (
     <div className="site">
-          <div class="container">
-        <div class="header">
-            <div class="header_logo">
-                <div class="header_logo_for">
-                    <div class="header_logo_icon"></div>
-                    <div class="header_logo_text">Halloween</div>
-                </div>
-            </div>
-            <div class="header_menu">
-                <div class="header_menu_text">
-                  <div class="menu_text">
-                    <div class="header_menu_text_inside">New</div></div>
-                  </div>
-                <div class="header_menu_text">
-                  <div class="menu_text">
-                    <div class="header_menu_text_inside">New</div></div>
-                  </div>
-                <div class="header_menu_text">
-                  <div class="menu_text">
-                    <div class="header_menu_text_inside">New</div></div>
-                  </div>
-                <div class="header_menu_text">
-                  <div class="menu_text">
-                    <div class="header_menu_text_inside">New</div></div>
-                  </div>
-                <div class="header_menu_text">
-                  <div class="for_btn">
-                    <div class="text_in_btn">Support</div>
-                  </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="kost"></div>
-    </div>
-    <div class="container">
-        <div class="main_menu">
-            <div class="main_img">
-                <div class="main_img_img"></div>
-            </div>
-            <div class="text_place">
-                <div class="top_text"><div class="top_text_text">#TOP1 SCARIEST GHOSTS 2020</div></div>
-                <div class="middle_text">UOOOO  TRICK OR  TREAT </div>
-                <div class="bottom_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate consectetur provident asperiores ab reprehenderit, veritatis maiores earum voluptas eligendi magnam assumenda voluptates nesciunt! Nihil ea provident dolore expedita voluptatem aspernatur!</div>
-                <div class="main_menu_buttons">
-                    <div class="first_button"><div class="frst_button"><div class="ctr">Look now</div></div></div>
-                    <div class="second_button"><div class="cs">Track Record</div></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="bot">
-            <div class="add"></div>
-            <div class="bot_col">
-                <div class="bot_col_title">About</div>
-                <div class="bot_col_default">aaaa</div>
-            </div>
-            <div class="bot_col">
-                <div class="bot_col_title">About</div>
-                <div class="bot_col_default">aaaa</div>
-            </div>
-            <div class="bot_col">
-                <div class="bot_col_title">About</div>
-                <div class="bot_col_default">aaa</div>
-            </div>
-            <div class="bot_col">
-                <div class="bot_col_title">About</div>
-                <div class="bot_col_default">aaa</div>
-            </div>
-            <div class="addd"></div>
-
-        </div>
-    </div>
+      <div className="site__wrapper">
         <div className="site__menu">
-            Меню
+          <Menu />
         </div>
         <div className="site__content">
-          <div className="content__slider">
-              Слайдер
+          <div className="site__slider">
+            <div className="slider__list">
+              <div className="slider__block">
+                <div className="block__image">
+                  <Image
+                    src={home1}
+                    width="460px"
+                    height="500px"
+                  />
+                </div>
+                <div className="block__info">
+                  <h1 class="info__title">
+                    UOOOO
+                    <br />
+                    TRICK OR
+                    <br />
+                    TREAT!!
+                  </h1>
+                  <p class="info__description">
+                    Hi, I'm Reiza, people call me "El Labu". I am currently trying
+                    to learn something new, building my own bike with parts made
+                    only in Malaysia.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="slider__menu">
+              <div className="menu__mark"></div>
+              <div className="menu__mark"></div>
+            </div>
           </div>
         </div>
       </div>
-    
+    </div>
   )
 }
-
